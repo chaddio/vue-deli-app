8646f9916eb276704c249c23fcdefed2a276d847
Chad Marshall <cmarshall@speareducation.com>
2019-06-24 07:59:35 -0700

    initial commit

---
+++ src/views/Test.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="test">
+        <h1>Ben Test</h1>
+
+        test: {{ test }}
+        params: {{ params }}
+
+        envTest: {{ envTest }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test',
+        data() {
+            return {
+                test: '123'
+            }
+        },
+        computed: {
+            params() {
+                return this.$route.params
+            },
+            envTest() {
+                return process.env.VUE_APP_TEST_PARAM
+            }
+        },
+        mounted() {
+            console.log('this.route.params', this.$route.params)
+
+            console.log('process.env.VUE_APP_TEST_PARAM', process.env.VUE_APP_TEST_PARAM)
+        }
+    }
+</script>
